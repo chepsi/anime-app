257140d8867ddcfdd76f5b67f331f5bddd575ba0
chepsi <evanschepsiror@gmail.com>
2023-09-06 13:40:06 +0300

    Feature: enhance ui

--- presentation/src/main/java/chepsi/anime/app/presentation/screens/search/components/SearchMainComponent.kt
+++ presentation/src/main/java/chepsi/anime/app/presentation/screens/search/components/SearchMainComponent.kt
@@ -4,21 +4,23 @@ import android.net.Uri
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.PickVisualMediaRequest
 import androidx.activity.result.contract.ActivityResultContracts
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Button
 import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.OutlinedButton
 import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import chepsi.anime.app.presentation.screens.search.SearchScreenState
 import chepsi.anime.app.presentation.theme.AnimeAppTheme
 import coil.compose.AsyncImage
@@ -57,15 +59,19 @@ fun SearchMainComponent(
             )
         }
         Column(
-            modifier = Modifier,
-            verticalArrangement = Arrangement.Center
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(20.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
             OutlinedTextField(
                 value = screenState.imageUrl ?: "",
                 onValueChange = { onUpdateUrl(it) },
                 singleLine = true,
-                label = { Text("Anime Image Url") }
+                label = { Text("Image Url") },
+                modifier = Modifier.fillMaxWidth()
             )
+            Text("-- OR --")
             OutlinedButton(
                 onClick = {
                     singlePhotoPickerLauncher.launch(
@@ -104,8 +110,14 @@ fun SearchMainComponent(
             }
         }
 
-        Text(screenState.title)
-        Text(screenState.episode)
+
+        if (screenState.title.isNotBlank() && !screenState.isError) {
+            Text("Title: ${screenState.title}", modifier = Modifier.padding(20.dp))
+            Text("Episode: ${screenState.episode}", modifier = Modifier.padding(20.dp))
+        }
+        if (screenState.isError && screenState.errorMessage != null) {
+            Text("Error: ${screenState.errorMessage}", modifier = Modifier.padding(20.dp))
+        }
     }
 }
 
